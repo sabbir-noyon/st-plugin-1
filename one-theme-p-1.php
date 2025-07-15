d9dba213e2b7f786ba59fadc8f5d45b096c4e3d4
Sabbir-Noyon <sabbir.noyon.001@gmail.com>
2025-07-16 01:34:34 +0600

    Refactored plugin code

--- one-theme-p-1.php
+++ one-theme-p-1.php
@@ -1,137 +1,116 @@
 <?php
 /**
- * Plugin Name: One Theme P1
- * Plugin URI: https://wordpress.org/plugins/one-theme-p-1/
- * Description: This is the intro plugin
- * Version: 1.0.0
- * Author: Sabbir Noyon
- * Author URI: https://wordpress.org/plugins/one-theme-p-1/
+ * Plugin Name:       One Theme P1
+ * Plugin URI:        https://wordpress.org/plugins/one-theme-p-1/
+ * Description:       This is a content modifier plugin.
+ * Version:           1.0.0
+ * Author:            Sabbir Noyon
+ * Author URI:        https://www.github.com/sabbir-noyon/
  */
 
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
+/**
+ * Class Custom_Title_Content_Modifier
+ *
+ * Appends author name to the title and displays word count and reading time in content.
+ */
+class Custom_Title_Content_Modifier {
 
-// Procedural
+	/**
+	 * Constructor to initialize hooks.
+	 */
+	public function __construct() {
+		add_action( 'init', array( $this, 'initialization' ) );
+	}
 
+	/**
+	 * Registers content filters on init.
+	 */
+	public function initialization() {
+		add_filter( 'the_title', array( $this, 'change_title' ) );
+		add_filter( 'the_content', array( $this, 'change_content' ) );
+	}
 
-// add_filter('the_title', 'change_title');
+	/**
+	 * Modifies the post title.
+	 *
+	 * @param string $new_title The original post title.
+	 *
+	 * @return string Modified title.
+	 */
+	public function change_title( $new_title ) {
+		return $new_title . ' - John Doe';
+	}
 
+	/**
+	 * Appends word count and reading time to the content.
+	 *
+	 * @param string $new_content The original post content.
+	 *
+	 * @return string Modified content.
+	 */
+	public function change_content( $new_content ) {
+		$content    = wp_strip_all_tags( $new_content );
+		$word_count = str_word_count( $content );
+		$time       = ceil( $word_count / 200 );
 
-// function change_title($new_title)
-// {
-//     return $new_title . " - " . "Sabbir Noyon";
-
-
-// }
-
-// add_filter('the_content', 'change_content');
-
-// function change_content($new_content)
-// {
-
-//     $content = strip_tags($new_content);
-//     $word_count = str_word_count($content);
-
-//     $time = ceil($word_count / 200);
-
-//     return $new_content . "Total Word is {$word_count}, & Approximately reading time {$time} Minutes";
-
-
-// }
-
-
-
-// OOP 
-
-class Custom_Title_Content_Modifier
-{
-
-    public function __construct()
-    {
-        add_action('init', array($this, 'initialization'));
-
-
-    }
-
-    public function initialization()
-    {
-        add_filter('the_title', array($this, 'change_title'));
-        add_filter('the_content', array($this, 'change_content'));
-    }
-
-    function change_title($new_title)
-    {
-        return $new_title . " - " . "Sabbir Noyon";
-
-
-    }
-
-
-    function change_content($new_content)
-    {
-
-        $content = strip_tags($new_content);
-        $word_count = str_word_count($content);
-
-        $time = ceil($word_count / 200);
-
-        return $new_content . "Total Word is {$word_count}, & Approximately reading time {$time} Minutes";
-
-
-    }
-
-
-
-
-
-
+		return $new_content . " Total words: {$word_count}, Approx. reading time: {$time} minutes.";
+	}
 }
 
 new Custom_Title_Content_Modifier();
 
+/**
+ * Class Name_Modifier
+ *
+ * Another content modifier class.
+ */
+class Name_Modifier {
 
-// // OOP
+	/**
+	 * Constructor to initialize hooks.
+	 */
+	public function __construct() {
+		add_action( 'init', array( $this, 'initialization' ) );
+	}
 
-// class Name_Modifier{
+	/**
+	 * Registers content filters.
+	 */
+	public function initialization() {
+		add_filter( 'the_title', array( $this, 'change_title' ) );
+		add_filter( 'the_content', array( $this, 'change_content' ) );
+	}
 
-//     function __construct()
-//     {
+	/**
+	 * Modifies the post title.
+	 *
+	 * @param string $new_title The original post title.
+	 *
+	 * @return string Modified title.
+	 */
+	public function change_title( $new_title ) {
+		return $new_title . ' WP';
+	}
 
-//         add_action('init', array($this, 'initialization'));
+	/**
+	 * Appends word count and reading time to the content.
+	 *
+	 * @param string $new_content The original content.
+	 *
+	 * @return string Modified content.
+	 */
+	public function change_content( $new_content ) {
+		$content    = wp_strip_all_tags( $new_content );
+		$word_count = str_word_count( $content );
+		$time       = ceil( $word_count / 200 );
 
-//     }
+		return $new_content . " Total words: {$word_count}, Approx. reading time: {$time} minutes.";
+	}
+}
 
-//     function initialization()
-//     {
-
-//        add_filter('the_title', array($this, 'change_title'));
-
-//        add_filter('the_content', array($this, 'change_content'));
-
-//     }
-
-//     function change_title($new_title)
-//     {
-//         return $new_title . "sabbiiiiiiiiiiiiiiiiiiiiiir";
-
-//     }
-
-
-
-//     function change_content($new_content)
-//     {
-//        $after_strip = strip_tags($new_content);
-
-//        $word_count = str_word_count($after_strip);
-
-//        $time = ceil($word_count/200);
-
-//        return $new_content ."Total Word is {$word_count }, Approx Reading Time {$time} minutes";
-
-//     }
-
-
-
-
-// }
-
-// new Name_Modifier();
+new Name_Modifier();
--- qr-code-p-1.php
+++ qr-code-p-1.php
@@ -1,43 +1,61 @@
 <?php
-
 /**
- * Plugin Name: qr-code-p-1
- * Plugin URI: https://wordpress.org/plugins/qr-code-p-1/
- * Description: This is QR Code plugin
- * Version: 1.0.0
- * Author: Sabbir Noyon
- * Author URI: https://wordpress.org/plugins/qr-code-p-1/
+ * Plugin Name:       QR Code P1
+ * Plugin URI:        https://wordpress.org/plugins/qr-code-p-1/
+ * Description:       This is a QR Code generator plugin.
+ * Version:           1.0.0
+ * Author:            Sabbir Noyon
+ * Author URI:        https://www.github.com/sabbir-noyon/
  */
 
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
- class QR_Code_Generator{
+/**
+ * Class QR_Code_Generator
+ *
+ * Appends a QR code of the current post/page URL at the end of the content.
+ */
+class QR_Code_Generator {
 
-    public function __construct()
-    {
-        add_action('init', [$this, 'initialize']);
+	/**
+	 * Constructor.
+	 * Hooks into the init action.
+	 */
+	public function __construct() {
+		add_action( 'init', array( $this, 'initialize' ) );
+	}
 
-    }
+	/**
+	 * Registers content filter on init.
+	 */
+	public function initialize() {
+		add_filter( 'the_content', array( $this, 'qr_code_container' ) );
+	}
 
-    public function initialize()
-    {
-        add_filter('the_content', [$this, 'qr_code_container']);
+	/**
+	 * Appends a QR code to the post/page content.
+	 *
+	 * @param string $content The original content.
+	 *
+	 * @return string Modified content with QR code.
+	 */
+	public function qr_code_container( $content ) {
+		if ( ! is_singular() || ! in_the_loop() || ! is_main_query() ) {
+			return $content;
+		}
 
+		$current_page_url = esc_url( get_permalink() );
+		$qr_code_url      = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' . $current_page_url;
 
-    } 
+		$qr_html  = '<p>Scan this QR code to visit this page:</p>';
+		$qr_html .= '<p><img src="' . esc_url( $qr_code_url ) . '" alt="QR Code" /></p>';
 
-    public function qr_code_container($received_contents)
-    {
-        $current_page = get_permalink();
-        $qr_code_image = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example'.$current_page;
+		return $content . $qr_html;
+	}
+}
 
-        return $received_contents. "<p>Scanner: <img src='{$qr_code_image}'></p>";
-        // "<p>This is my F {$current_page}</p>";
-    }
-
-
- }
-
-
+// Initialize the plugin.
 new QR_Code_Generator();
-
-
